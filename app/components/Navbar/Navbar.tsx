61a9e7d1982b55e510bb8ca81f894c45f127db33
Aslah <aslahfw@gmail.com>
2025-10-13 17:21:34 +0530

    navbar complete

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,3 +1,28 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav className="h-[50px] border-b-2">Navbar</nav>;
+  return (
+    <nav className="px-6">
+      <div className="py-6 flex justify-between items-center">
+        <div>
+          <Link href="/" className="text-5xl font-bold">
+            BLOGSPOT
+          </Link>
+        </div>
+        <div>
+          <ul className="flex gap-6 mt-4">
+            <li className="text-[20px]">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="text-[20px]">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="text-[20px]">
+              <Link href="/contact">Hire me</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
