d6ba84f21843bebb96c009acc391ba1048c38e35
Aslah <aslahfw@gmail.com>
2025-10-13 17:23:26 +0530

    completed footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,25 +1,24 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-3 md:flex-row justify-between items-center p-6 bg-gray-300 text-black mt-12">
+    <footer className="flex flex-col gap-3 md:flex-row justify-between items-center p-6 bg-gray-200 text-black mt-12">
       <div className="">
         <h3>Thank for coming</h3>
       </div>
 
       {/* email */}
       <div>
-        <Link href="mailto:aslahaboobacker2@gmail.com" className="text-red-400">
-          aslahaboobacker2@gmail.com
+        <Link href="mailto:aslahaboobacker2@gmail.com">
+          Mail me on: aslahaboobacker2@gmail.com
         </Link>
       </div>
 
       {/* Social Media Handler */}
-      <div>
+      {/* <div>
         <span>insta</span>          
         <span>FB</span>          
-      </div>
+      </div> */}
     </footer>
   );
 }
